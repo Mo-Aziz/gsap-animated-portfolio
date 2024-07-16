7f693c4b03fa4f21c44c34403bfdee4f6bb36a23
Mo-Aziz <mohamed.aref32@gmail.com>
2024-07-16 11:27:12 -0500

    added the mouse affects on hover with gsap

--- src/slices/ContentIndex/index.tsx
+++ src/slices/ContentIndex/index.tsx
@@ -2,6 +2,8 @@ import Bounded from "@/components/Bounded";
 import Heading from "@/components/Heading";
 import { Content, isFilled } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import ContentList from "./ContentList";
+import { createClient } from "@/prismicio";
 
 /**
  * Props for `ContentIndex`.
@@ -11,7 +13,16 @@ export type ContentIndexProps = SliceComponentProps<Content.ContentIndexSlice>;
 /**
  * Component for "ContentIndex" Slices.
  */
-const ContentIndex = ({ slice }: ContentIndexProps): JSX.Element => {
+const ContentIndex = async ({
+  slice,
+}: ContentIndexProps): Promise<JSX.Element> => {
+  const client = createClient();
+  const blogPosts = await client.getAllByType("blog_post");
+  const projects = await client.getAllByType("project");
+
+  const contentType = slice.primary.content_type || "Blog";
+  const items = contentType === "Blog" ? blogPosts : projects;
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
@@ -22,11 +33,16 @@ const ContentIndex = ({ slice }: ContentIndexProps): JSX.Element => {
         {slice.primary.heading}
       </Heading>
       {isFilled.richText(slice.primary.description) && (
-        <div className="prose prose-xl   prose-invert mb-10">
+        <div className="prose prose-xl prose-invert mb-10">
           <PrismicRichText field={slice.primary.description} />
         </div>
       )}
-      <ContentList />
+      <ContentList
+        items={items}
+        contentType={contentType}
+        viewMoreText={slice.primary.view_more_text}
+        fallbackItemImage={slice.primary.fallback_item_image}
+      />
     </Bounded>
   );
 };
